44751e8f96306fd1f16192b6b3895d3fc87ab300
Shubhali Sinha <shubhali@mindrops.com>
2021-05-27 15:33:02 +0530

    File updated again

--- mysqlCRUD.js
+++ mysqlCRUD.js
@@ -27,7 +27,7 @@ app.listen(port, () => {
 app.get('/employees', (req,res) => {
     mysqlConnection.query('SELECT * FROM employee', (err, rows, fields) =>{
     if(!err)
-    console.log(rows);
+    res.json({Data_from_table_employee: rows });
     else
     console.log(err);
   })
@@ -37,7 +37,7 @@ app.get('/employees', (req,res) => {
 app.get('/employees/:id', (req,res) => {
     mysqlConnection.query('SELECT * FROM employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) =>{
     if(!err)
-    console.log(rows);
+    res.json({Data_related_to_the_given_id: rows });
     else
     console.log(err);
   })
@@ -47,34 +47,38 @@ app.get('/employees/:id', (req,res) => {
 app.delete('/employees/:id', (req,res) => {
     mysqlConnection.query('DELETE FROM employee WHERE EmpID =?', [req.params.id], (err, rows, fields) =>{
     if(!err)
-    console.log('Record deleted');
+    res.status(200).json({status:'data deleted'});
     else
     console.log(err);
   })
 });
 
 //Insert new data
-app.post('/insert', (req,res) => {
+app.post('/employees/insert', (req,res) => {
     
-    let sql = 'INSERT INTO employee SET ?'
+    let sql = 'INSERT INTO employee SET ?';
     let post = {
         Name: req.body.Name,
         EmpCode: req.body.EmpCode,
         Salary: req.body.Salary
     }
 
-    mysqlConnection.query(sql,post,(err,res) =>{
-        if (err) throw err;
-        console.log("1 record inserted");
-    });
+    mysqlConnection.query(sql,post,(err,rows,fields) =>{
+        if(!err){
+         res.status(200).json({status:'data inserted'});
+        }
+        else
+        console.log(err);
+    })
 });
 
-    app.patch('/update/:EmpID',(req,res) => {
+    app.patch('/employees/update/:EmpID',(req,res) => {
     
         mysqlConnection.query("UPDATE employee SET Name=?,EmpCode=?,Salary=? WHERE EmpID=?",[req.body.Name,req.body.EmpCode,req.body.Salary,req.params.EmpID],(err,results) => {
-            if (err) 
-            console.log(err);
+            if (!err) 
+            res.status(200).json({status:'data updated'});
             
             res.end(JSON.stringify(results));
+            
         });
     });
